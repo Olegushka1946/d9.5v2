b418b52b4d35476aa5eefdf16778fd543ccd9652
Olegushka1946 <126393443+Olegushka1946@users.noreply.github.com>
2023-09-29 11:52:59 +0300

    Update settings.py

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -162,7 +162,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_FORMS = {'signup': 'sign.models.BasicSignupForm'}
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
